e6633214012e7eb78fa892a9f5a072acf53ed17f
Shichen Xu <shichen@admin.cluster.local>
2019-09-09 21:04:57 +0800

    update

--- main.py
+++ main.py
@@ -12,8 +12,3 @@ if __name__ == '__main__':
     rec.run()
     if config['SHOW']['PLOT'] == 'T':
         rec.show()
-
-# for _ in range(100):
-#     s = np.random.randint(0, 9722, 10)
-#     a = np.random.randint(0, 9222)
-#     print(env.get_reward(s, a))
